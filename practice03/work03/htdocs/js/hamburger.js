d4e16b3171d89fe5fe2331e4afffe76a3f39e56c
kamaboko <hiroki11nagato@icloud.com>
2025-10-26 08:42:39 +0900

    3 complete

--- practice03/work03/htdocs/js/hamburger.js
+++ practice03/work03/htdocs/js/hamburger.js
@@ -1,2 +1,7 @@
 const buttonTrigger = document.querySelector('.hamburger');
 const topNavigation = document.querySelector('.topNav');
+
+buttonTrigger.addEventListener('click', function(){
+    topNavigation.classList.toggle('open');
+    buttonTrigger.classList.toggle('active')
+});
